94b93e5f300b94012242183bf0139c2a3ee393ee
erndidi <erndidi@outlook.com>
2017-08-27 11:26:41 -0400

    Add project files.

---
+++ LFC/Controllers/ArticleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LFC.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetSalesCopy()
+        {
+            return View();
+        } // GET: Article/Details/5
+
+        public ActionResult GetNewsCopyLinks()
+        {
+            return View();
+        }
+
+        public ActionResult BeyondTheGame()
+        {
+            return View();
+        }
+
+        public ActionResult GetHopeCenter()
+        {
+            return View();
+        }
+
+        public ActionResult GetSpaceCenter()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ LFC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using LFC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LFC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="LFC";
+return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Contact contact = new Models.Contact();
+            return View(contact);
+        }
+        [HttpPost]
+        public void SendEmail(Contact contact) 
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(contact.EmailAddress);
+
+            message.To.Add(new MailAddress("leapforwardcopy@gmail.com"));
+
+            message.Subject = contact.Subject;
+            message.Body = contact.Message;
+
+            SmtpClient client = new SmtpClient();
+            client.Send(message);
+        }
+    }
+}
